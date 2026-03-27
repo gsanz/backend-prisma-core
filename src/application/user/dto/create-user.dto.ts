a325aeb46fa335a4e9e85a60cd6fc81a1ea94765
Gorka <gsanz5@tragsa.es>
2026-03-27 15:19:54 +0100

    first validation added

--- src/application/user/dto/create-user.dto.ts
+++ src/application/user/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
-// application/user/dto/create-user.dto.ts
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
   name: string;
+
+  @IsEmail()
   email: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina campos no definidos en DTO
+      forbidNonWhitelisted: true, // error si envían campos extra
+      transform: true, // transforma tipos automáticamente
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
