4dbc3fe527a5c66cba53c5e16a144d7dcbbd44fd
Gorka <gsanz5@tragsa.es>
2026-03-27 12:02:47 +0100

    test correction added

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './infraestructure/test/controllers/app.controller';
+import { AppService } from './domain/test/services/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
