8ecb6822a39cce9b14c7469e8b728fb3850b6978
Gorka <gsanz5@tragsa.es>
2026-09-11 11:53:00 +0200

    not duplicated user

--- src/application/user/use-cases/create-user.usecase.ts
+++ src/application/user/use-cases/create-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, BadRequestException } from '@nestjs/common';
 import { USER_REPOSITORY } from '../../../domain/user/repositories/user.repository';
 import type { UserRepository } from '../../../domain/user/repositories/user.repository';
 import { User } from '../../../domain/user/entities/user.entity';
@@ -13,6 +13,10 @@ export class CreateUserUseCase {
   ) {}
 
   async execute(dto: CreateUserDto) {
+    const existingUser = await this.userRepo.findByEmail(dto.email);
+    if (existingUser) {
+      throw new BadRequestException('Ya existe un usuario con ese email');
+    }
     const user = User.create(uuid(), dto.name, dto.email, dto.password);
     return this.userRepo.save(user);
   }
--- src/application/user/use-cases/update-user.usecase.ts
+++ src/application/user/use-cases/update-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject, NotFoundException } from '@nestjs/common';
+import { Injectable, Inject, NotFoundException, BadRequestException } from '@nestjs/common';
 import { USER_REPOSITORY } from '../../../domain/user/repositories/user.repository';
 import type { UserRepository } from '../../../domain/user/repositories/user.repository';
 
@@ -13,21 +13,25 @@ export class UpdateUserUseCase {
   ) {}
 
   async execute(id: string, dto: UpdateUserDto) {
-    // 1. comprobar que existe
     const existingUser = await this.userRepo.findById(id);
 
     if (!existingUser) {
       throw new NotFoundException('No hay ningún id asociado');
     }
 
-    // 2. mapear DTO → dominio
+    if (dto.email && dto.email !== existingUser.getEmail()) {
+      const emailTaken = await this.userRepo.findByEmail(dto.email);
+      if (emailTaken) {
+        throw new BadRequestException('Ya existe un usuario con ese email');
+      }
+    }
+
     const data: UpdateUserData = {
       name: dto.name,
       email: dto.email,
       password: dto.password,
     };
 
-    // 3. actualizar
     return this.userRepo.update(id, data);
   }
 }
